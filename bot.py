5983ce89c54a5d3531260397f2ae322a7c07d4f4
AaronCGoidel <agoidel2019@mka.org>
2019-04-30 23:42:25 -0400

    fixed greeting message spacing

--- bot.py
+++ bot.py
@@ -37,8 +37,9 @@ def sendGreeting():
   user_id = os.environ["TARGET_ID"]
   user_info = requests.get("https://slack.com/api/im.open?token="+TOKEN+"&user="+user_id).json()
   CHANNEL = user_info["channel"]["id"]
-
-  message = "Hello," + getName(user_id) + ". Thank you for subscribing to CAT FACTS!" + "You will receive one random interesting feline fact every hour."
+  greeting = "Hello, " + getName(user_id) + ". "
+  body = "Thank you for subscribing to CAT FACTS! " + "You will receive one random interesting feline fact every hour."
+  message = greeting + body
 
   message_data = {
     'token': TOKEN,
